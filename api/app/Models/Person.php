1351c47b61360e8088e2c389fde18fa340fa2d0f
Jeff Todnem <badbrownbox@gmail.com>
2022-06-10 21:47:03 -0800

    .

--- api/app/Models/Person.php
+++ api/app/Models/Person.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use App\Extensions\Model;
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
+use Jenssegers\Mongodb\Eloquent\SoftDeletes;
+use MoeenBasra\LaravelPassportMongoDB\HasApiTokens;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
 class Person extends Model implements AuthenticatableContract, AuthorizableContract
 {
